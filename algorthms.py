22020b30286e76f2bea533ce51211ead2f1eac31
patryk-kluzik <patrykkluzik99@gmail.com>
2022-04-14 20:43:41 +0100

    reformatted function to use list comprehension instead

--- algorthms.py
+++ algorthms.py
@@ -1,11 +1,8 @@
-from random import choice, randint, randrange
+from random import randint
 
 
 def get_random_list(list_size: int, min_value: int, max_value: int) -> list:
-    random_list = []
-    for i in range(list_size):
-        random_list.append(randint(min_value, max_value))
-    return random_list
+    return [randint(min_value, max_value) for _ in range(list_size)]
 
 
 random_unsorted_list = get_random_list(list_size=10, min_value=0, max_value=100)
