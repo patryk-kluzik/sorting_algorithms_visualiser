19d857afba198a31f99a2fbe8ac69231e6faed24
patryk-kluzik <patrykkluzik99@gmail.com>
2022-04-14 21:17:37 +0100

    removed testing code, feature functionally completed and is ready to be merged into the master branch

--- algorthms.py
+++ algorthms.py
@@ -3,7 +3,3 @@ from random import randint
 
 def get_random_list(list_size: int, min_value: int, max_value: int) -> list:
     return [randint(min_value, max_value) for _ in range(list_size)]
-
-
-random_unsorted_list = get_random_list(list_size=10, min_value=0, max_value=100)
-print(random_unsorted_list)
