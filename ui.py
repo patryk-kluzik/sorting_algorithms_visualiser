691601061626e2dd046a2c5ecc2783ebd34ea968
Patryk Kluzik <patrykkluzik99@gmail.com>
2022-04-16 16:21:59 +0100

    reformatted ui to use classes

--- ui.py
+++ ui.py
@@ -1,16 +1,16 @@
 from tkinter import *
 
-window = Tk()
-window.title("Sorting Algorithm Visualiser")
-window.config(bg="light gray", width=900, height=600)
-window.resizable(width=False, height=False)
-window.minsize(width=900, height=600)
 
-list_size_bar = Scale(orient=HORIZONTAL)
-list_size_bar.config(resolution=5, from_=0, to=50)
-list_size_bar.grid(row=0,column=1)
+class VisualiserUI:
 
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Sorting Algorithm Visualiser")
+        self.window.config(bg="light gray", width=900, height=600)
+        self.window.resizable(width=False, height=False)
+        self.window.minsize(width=900, height=600)
+        self.list_size_bar = Scale(orient=HORIZONTAL)
+        self.list_size_bar.config(resolution=5, from_=0, to=50)
+        self.list_size_bar.grid(row=0, column=1)
 
-
-
-window.mainloop()
+        self.window.mainloop()
