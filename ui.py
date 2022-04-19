b314b9af4314af2fd471f686f9ea03bb098f26d9
patryk-kluzik <patrykkluzik99@gmail.com>
2022-04-19 14:10:01 +0100

    bubble sort visualisation completed

--- ui.py
+++ ui.py
@@ -1,11 +1,13 @@
 import tkinter.ttk
-from algorthms import get_random_list
+from algorthms import *
 from tkinter import *
 
 
 class VisualiserUI:
 
     def __init__(self):
+        self.array = []
+
         # ------------ root --------------#
         self.window = Tk()
         self.window.title("Sorting Algorithm Visualiser")
@@ -15,7 +17,7 @@ class VisualiserUI:
 
         # ------------ Frame --------------#
         self.frame = Frame(master=self.window, width=900, height=200, bg="grey")
-        self.frame.grid(row=0, column=0, padx=5, pady=5,)
+        self.frame.grid(row=0, column=0, padx=5, pady=5, )
 
         # ------------ Canvas --------------#
         self.canvas = Canvas(master=self.window, width=900, height=400, bg="white")
@@ -35,11 +37,11 @@ class VisualiserUI:
         self.max_value_scale.grid(row=1, column=2, padx=20, pady=5)
 
         self.sort_speed_scale = Scale(master=self.frame, orient=HORIZONTAL, length=200)
-        self.sort_speed_scale.config(resolution=0.1, from_=0.1, to=2, label="Select Speed [s]")
+        self.sort_speed_scale.config(resolution=0.05, from_=0.05, to=1, label="Select Speed [s]")
         self.sort_speed_scale.grid(row=0, column=2, padx=20, pady=5)
 
         # ------------ Buttons --------------#
-        self.generate_button = Button(master=self.frame, text="Generate Array", command=self.generate_array)
+        self.generate_button = Button(master=self.frame, text="Generate Array", command=self.get_array_values)
         self.generate_button.grid(row=1, column=3)
 
         self.run_algorithm_button = Button(master=self.frame, text="Run algorith", command=self.run_algorithm)
@@ -54,17 +56,20 @@ class VisualiserUI:
         self.algorithm_menu = tkinter.ttk.Combobox(master=self.frame,
                                                    textvariable=self.algorithm_value,
                                                    values=["Bubble Sort", "Merge Sort"])
-        self.algorithm_menu.grid(row=0, column=1,padx=5, pady=5)
+        self.algorithm_menu.grid(row=0, column=1, padx=5, pady=5)
         self.algorithm_menu.current(0)
 
         self.window.mainloop()
 
-    def generate_array(self):
-        self.canvas.delete("all")
-        random_list = get_random_list(list_size=self.list_size_scale.get(),
-                                      min_value=self.min_value_scale.get(),
-                                      max_value=self.max_value_scale.get())
+    def get_array_values(self):
+        self.array = get_random_list(list_size=self.list_size_scale.get(),
+                                     min_value=self.min_value_scale.get(),
+                                     max_value=self.max_value_scale.get())
+        self.draw_array(random_list=self.array, color_array=["red" for x in range(self.list_size_scale.get())])
+        return self.array
 
+    def draw_array(self, random_list, color_array):
+        self.canvas.delete("all")
         offset = 30
         c_width = 900 - (offset / 2)
         c_height = 400
@@ -78,9 +83,10 @@ class VisualiserUI:
             y0 = c_height - array_height * 370
             x1 = (i + 1) * x_width
             y1 = c_height
-            self.canvas.create_rectangle(x0, y0, x1, y1, fill="red")
+            self.canvas.create_rectangle(x0, y0, x1, y1, fill=color_array[i])
             self.canvas.create_text(x0 + 2, y0, anchor=SW, text=str(random_list[i]))
 
-    def run_algorithm(self):
-        print(f"{self.algorithm_value.get()}")
+        self.window.update_idletasks()
 
+    def run_algorithm(self):
+        bubble_sort(self.array, draw_data=self.draw_array, sorting_speed=self.sort_speed_scale.get())
