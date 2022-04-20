d6c5ecbf2c36df21e16f9a955460fce7c4c2dcc0
patryk-kluzik <patrykkluzik99@gmail.com>
2022-04-20 10:17:20 +0100

    merge function drawing is improved - shows the specific sublists that are being sorted, also added some documentation to explain the sorting algorithms functions
    TODO: add more docstrings to explain these functions

--- algorthms.py
+++ algorthms.py
@@ -33,14 +33,57 @@ def bubble_sort(unsorted_list: list, draw_data, sorting_speed: float):
 
 
 def merge_sort(unsorted_list: list, left_index, right_index, draw_data, sorting_speed: float):
+    """
+    This function takes a list and splits it's into smaller sublists by calling itself recursively, which then are
+    merged by the merge function. The function finds the middle point to divide the array into two halves.
+    Then the function is recursively called for the first half, and again for the second half.
+    Finally, we call the merge function to merge the these halves, which sorts the sublist.
+
+    E.G.  For a list that has 8 elements, the left index is 0, middle is 3 and right is 7.
+
+    During the first recursion, the left_index is 0 while the right_index is 3. Here the middle_index is now 1.
+    The function calls itself, passing 0 and 1 and left and right indexes, but when the function calls itself again,
+    but as the middle index is now 0, the left_index isn't smaller than the right so the loop stops.
+    Then the second recursion for where the left is the middle_index + 1 and the right index is itself.
+    In this example the left index would be 2 (1+1) and the right would be 3. The function calls itself, but now the
+    middle index is (2+3) // 2 which returns 2. The function tried to call itself again, the left is 3 (2+1) and the
+    right is still 3, but as left is no longer smaller than the right, this stops the loop.
+
+    This then continues for the 2nd half of the array ( here the left array is middle + 1 and right is itself)
+    The left index here is then 4 and right is 7, with 5 being the middle array. This then repeats similarly to above,
+    until the list cannot be split anymore and the left_array is not longer smaller than the right_array
+
+
+
+    :param unsorted_list: a list of unsorted elements
+    :param left_index: the left most index (0)
+    :param right_index: the right most index (length of the array - 1)
+    :param draw_data: function which draws data in the UI class
+    :param sorting_speed: float that determines the delay of the sorting speed
+    """
     if left_index < right_index:
         middle_index = (left_index + right_index) // 2
+        print(left_index, middle_index, right_index)
         merge_sort(unsorted_list, left_index, middle_index, draw_data, sorting_speed)
         merge_sort(unsorted_list, middle_index + 1, right_index, draw_data, sorting_speed)
         merge(unsorted_list, left_index, middle_index, right_index, draw_data, sorting_speed)
 
 
 def merge(unsorted_list: list, left_index, middle_index, right_index, draw_data, sorting_speed: float):
+    """
+
+
+    :param unsorted_list: a list of unsorted elements
+    :param left_index: the left most index (0)
+    :param middle_index: the middle index of the array
+    :param right_index: the right most index (length of the array - 1)
+    :param draw_data: function which draws data in the UI class
+    :param sorting_speed: float that determines the delay of the sorting speed
+    :return:
+    """
+    draw_data(unsorted_list, get_colour_array(len(unsorted_list), left_index, middle_index, right_index))
+    time.sleep(sorting_speed)
+
     l_array = unsorted_list[left_index:middle_index + 1]
     r_array = unsorted_list[middle_index + 1:right_index + 1]
 
@@ -69,5 +112,19 @@ def merge(unsorted_list: list, left_index, middle_index, right_index, draw_data,
         r_array_index += 1
         sorted_index += 1
 
-    draw_data(unsorted_list, ["green" if x == r_array or x == l_array else "red" for x in range(len(unsorted_list))])
-    time.sleep(sorting_speed)
+    draw_data(unsorted_list,
+              ["green" if left_index <= x <= right_index else "red" for x in range(len(unsorted_list))])
+
+
+def get_colour_array(length, left_array, middle_array, right_array):
+    colour_array = []
+
+    for i in range(length):
+        if left_array <= i <= right_array:
+            if left_array <= i <= middle_array:
+                colour_array.append("yellow")
+            else:
+                colour_array.append("orange")
+        else:
+            colour_array.append("red")
+    return colour_array
