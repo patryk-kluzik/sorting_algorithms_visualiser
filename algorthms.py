5e5c54546ee4a29e71e1c7555a928e512b14d2de
patryk-kluzik <patrykkluzik99@gmail.com>
2022-04-22 18:50:56 +0100

    completed, added docstrings to explain all functions

--- algorthms.py
+++ algorthms.py
@@ -54,7 +54,6 @@ def merge_sort(unsorted_list: list, left_index, right_index, draw_data, sorting_
     until the list cannot be split anymore and the left_array is not longer smaller than the right_array
 
 
-
     :param unsorted_list: a list of unsorted elements
     :param left_index: the left most index (0)
     :param right_index: the right most index (length of the array - 1)
@@ -63,7 +62,6 @@ def merge_sort(unsorted_list: list, left_index, right_index, draw_data, sorting_
     """
     if left_index < right_index:
         middle_index = (left_index + right_index) // 2
-        print(left_index, middle_index, right_index)
         merge_sort(unsorted_list, left_index, middle_index, draw_data, sorting_speed)
         merge_sort(unsorted_list, middle_index + 1, right_index, draw_data, sorting_speed)
         merge(unsorted_list, left_index, middle_index, right_index, draw_data, sorting_speed)
@@ -71,16 +69,54 @@ def merge_sort(unsorted_list: list, left_index, right_index, draw_data, sorting_
 
 def merge(unsorted_list: list, left_index, middle_index, right_index, draw_data, sorting_speed: float):
     """
+    The entire unsorted list is passed into the function. The array is then drawn using the draw data function.
+    We split the array into the l_array and r_array (left array takes the values of the unsorted list between
+    left index and middle index  while right array is between middle index and right index).
+
+    Then set the l_array_index to 0 and right_array_index to 0, with the sorted_index equal to left_index parameter -
+    this determines where the sorting of the array will be taking place.
+    Check if the length of both l_array and r_array and longer than their set arrays (is true as index is set to 0
+    and the array is populated, but becomes false once the values is compared as the array index is increased and
+    becomes equal or greater than the length of the array - for array of size 1 this is true after one comparison).
+
+    If the element of the l_array is smaller or equal to the element of the r_array at their indices, set the value of
+    the unsorted_list at index sorted_index equal to the l_array at l_array_index - e.g. if the l_array is [1] and
+    r_array is [7] and sorted_index is 0, then we set the unsorted_list at index 0 to l_array at index 0.
+    Therefor the array remains unchanged while l_array_index and sorted_index are increased by 1.
+
+    Otherwise, (if the element of the l_array is greater than r_array) we set the value of the unsorted_list at index
+    of sorted_index (0) equal to the r_array at r_array index (0) - e.g. if l_array_is [10] amd r_array is [7] and
+    sorted_index is 0, then we set the unsorted_list at index 0 to r_array at index r_array 0.
+    Therefor the array now has the copied the [7] into the first position, but the 2nd array is still not swapped.
+    The first 2 elements of the unsorted_list go from [10,7] to [7,7].
+
+    After this, the array_index and sorted_index are increased by 1, and these checks are continued until
+    the array_index is no longer smaller than the list of that array (e.g. if the l_array and r_array have only 1
+    element, the loop only happens once, but is either array has more than 1 element, this can loop several times)
+
+    Then we check if the l_array_index and r_array_index are smaller than the length of these arrays.
+    If the previous loop is exited because one of the array is no gone through, then the other array will have its
+    array index smaller than it's length, where this loop would then happen. In this loop we check for remaining
+    elements of that index and set the value of the elements in the unsorted list to the remaining elements of that
+    array, increasing both sorted_index and array_index by 1 until we've gone through them all (and the array_index is
+    no longer smaller than its length). This would ensure the values of the elements are swapped and the sublist is
+    sorted. (e.g. if the r_array was smaller than the l_array, then l_array_index would be 0, r_array_index would be 1 and
+    sorted_index would be 1. Therefor, the l_array_index WOULD be smaller than the l_array length, where we would set
+    the element at index sorted_index (1) of the unsorted_list equal to element of l_array at l_array_index (0) -)
+    so if l_array was [10] and r_array was [7], then after the unsorted_list copied the element of the r_array into
+    index 0, the first to elements go from [10,7] to [7,7]. This check would then set the index 1 of the unsorted_list
+    to the first element of the l_array , therefore unsorted_list goes from [7,7] to [7,10] completing the swap.)
 
 
     :param unsorted_list: a list of unsorted elements
-    :param left_index: the left most index (0)
-    :param middle_index: the middle index of the array
-    :param right_index: the right most index (length of the array - 1)
+    :param left_index: the left most index passed
+    :param middle_index: the middle index between the 2 indexes
+    :param right_index: the right most index passed
     :param draw_data: function which draws data in the UI class
     :param sorting_speed: float that determines the delay of the sorting speed
     :return:
     """
+
     draw_data(unsorted_list, get_colour_array(len(unsorted_list), left_index, middle_index, right_index))
     time.sleep(sorting_speed)
 
@@ -116,10 +152,28 @@ def merge(unsorted_list: list, left_index, middle_index, right_index, draw_data,
               ["green" if left_index <= x <= right_index else "red" for x in range(len(unsorted_list))])
 
 
-def get_colour_array(length, left_array, middle_array, right_array):
+def get_colour_array(length, left_array, middle_array, right_array) -> list:
+    """
+    This function provides a list of colours that are used in the draw function to display
+    what elements of the list are being compared. This function goes through all the elements
+    in the list to provide them with colour values. If the element at the index i between left_array and right_array,
+    we will colour it as the sublist. If the element at index i is between left_array and middle_array, we will
+    colour is as yellow (showing the l_array sublist), else (if i is not smaller than middle_index) we will colour
+    it orange (showing the r_array sublist). The rest of the elements will be coloured red, as they are not part of the
+    sublists that are being compared.
+
+
+
+    :param length: length of the array
+    :param left_array: left index of the sublist
+    :param middle_array: middle index between the indices
+    :param right_array: right index of the sublist
+    :return: list of colours for the sorting animation
+    """
     colour_array = []
 
     for i in range(length):
+        print(f"l: {left_array}", f"r: {right_array}", middle_array, f"i: {i}", colour_array)
         if left_array <= i <= right_array:
             if left_array <= i <= middle_array:
                 colour_array.append("yellow")
